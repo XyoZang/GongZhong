b750b80d220375a5b74c4102d2347cdee897664a
NXing <z1304242002@163.com>
2022-08-14 23:46:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def get_birthday():
   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
-  return (next - today + 1).days
+  return (next - today + datetime.timedelta(days=1)).days
 
 def get_ciba():
     url = "http://open.iciba.com/dsapi/"
