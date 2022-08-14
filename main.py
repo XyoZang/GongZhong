a1b78fcb235995a7a8c0dbf1a93f594bc4fe63c6
NXing <z1304242002@163.com>
2022-08-14 23:33:13 +0800

    调试

--- main.py
+++ main.py
@@ -25,13 +25,6 @@ app_secret = os.environ["APP_SECRET"]
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
-
-def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-  res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
-
 def get_weather(province, city):
     # 城市id
     try:
@@ -71,13 +64,7 @@ def get_birthday():
   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
-  return (next - today).days
-
-def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
+  return (next - today + 1).days
 
 def get_ciba():
     url = "http://open.iciba.com/dsapi/"
@@ -91,110 +78,17 @@ def get_ciba():
     note_ch = r.json()["note"]
     return note_ch, note_en
 
-def get_random_color():
-  return "#%06x" % random.randint(0, 0xFFFFFF)
-
-def send_message(to_user, access_token, city_name, weather, max_temperature, min_temperature, note_ch, note_en):
-    url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
-    week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
-    year = localtime().tm_year
-    month = localtime().tm_mon
-    day = localtime().tm_mday
-    today = datetime.date(datetime(year=year, month=month, day=day))
-    week = week_list[today.isoweekday() % 7]
-    # 获取在一起的日子的日期格式
-    love_year = int(config["love_date"].split("-")[0])
-    love_month = int(config["love_date"].split("-")[1])
-    love_day = int(config["love_date"].split("-")[2])
-    love_date = date(love_year, love_month, love_day)
-    # 获取在一起的日期差
-    love_days = str(today.__sub__(love_date)).split(" ")[0]
-    # 获取生日的月和日
-    birthday_month = int(config["birthday"].split("-")[1])
-    birthday_day = int(config["birthday"].split("-")[2])
-    # 今年生日
-    year_date = date(year, birthday_month, birthday_day)
-    # 计算生日年份，如果还没过，按当年减，如果过了需要+1
-    if today > year_date:
-        birth_date = date((year + 1), birthday_month, birthday_day)
-        birth_day = str(birth_date.__sub__(today)).split(" ")[0]
-    elif today == year_date:
-        birth_day = 0
-    else:
-        birth_date = year_date
-        birth_day = str(birth_date.__sub__(today)).split(" ")[0]
-    data = {
-        "touser": to_user,
-        "template_id": config["template_id"],
-        "url": "http://weixin.qq.com/download",
-        "topcolor": "#FF0000",
-        "data": {
-            "date": {
-                "value": "{} {}".format(today, week),
-                "color": "#00FFFF"
-            },
-            "city": {
-                "value": city_name,
-                "color": "#808A87"
-            },
-            "weather": {
-                "value": weather,
-                "color": "#ED9121"
-            },
-            "min_temperature": {
-                "value": min_temperature,
-                "color": "#00FF00"
-            },
-            "max_temperature": {
-              "value": max_temperature,
-              "color": "#FF6100"
-            },
-            "love_day": {
-              "value": love_days,
-              "color": "#87CEEB"
-            },
-            "birthday": {
-              "value": birth_day,
-              "color": "#FF8000"
-            },
-            "note_en": {
-                "value": note_en,
-                "color": "#173177"
-            },
-            "note_ch": {
-                "value": note_ch,
-                "color": "#173177"
-            }
-        }
-    }
-    headers = {
-      'Content-Type': 'application/json',
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
-    }
-    response = post(url, headers=headers, json=data).json()
-    if response["errcode"] == 40037:
-        print("推送消息失败，请检查模板id是否正确")
-    elif response["errcode"] == 40036:
-        print("推送消息失败，请检查模板id是否为空")
-    elif response["errcode"] == 40003:
-        print("推送消息失败，请检查微信号是否正确")
-    elif response["errcode"] == 0:
-        print("推送消息成功")
-    else:
-        print(response)
-
-
 if __name__ == "__main__":
 
     client = WeChatClient(app_id, app_secret)
 
     wm = WeChatMessage(client)
+    today = datetime.date.today()
     wea, temp, tempn = get_weather(province, city)
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     week = week_list[today.isoweekday() % 7]
     note_ch, note_en = get_ciba()
-    city_name = cityinfo.cityInfo[province][city]["AREAID"]
+    city_name = cityinfo.cityInfo[province][city]["city"]
     data = {
             "date": {
                     "value": "{} {}".format(today, week),
